064bc58f173cab9d418ef1041866d84a1dabcb5d
Андрей Смирный <smirnay2001@mail.ru>
2024-12-18 14:52:07 +0300

    Реализовал handler для получения всех VolunteerDto

--- src/PetHome.Application/Features/Read/VolunteerManegment/GetAllVolunteersWithPagination/GetAllVolunteersWithPaginationUseCase.cs
+++ src/PetHome.Application/Features/Read/VolunteerManegment/GetAllVolunteersWithPagination/GetAllVolunteersWithPaginationUseCase.cs
@@ -1,4 +1,26 @@
-﻿namespace PetHome.Application.Features.Read.VolunteerManegment.GetAllVolunteersWithPagination;
+﻿using CSharpFunctionalExtensions;
+using Microsoft.Extensions.Logging;
+using PetHome.Application.Database.Read;
+
+namespace PetHome.Application.Features.Read.VolunteerManegment.GetAllVolunteersWithPagination;
 public class GetAllVolunteersWithPaginationUseCase
 {
+    private readonly IReadDBContext _readDBContext;
+    private readonly ILogger<GetAllVolunteersWithPaginationUseCase> _logger;
+
+    public GetAllVolunteersWithPaginationUseCase(
+        IReadDBContext readDBContext,
+        ILogger<GetAllVolunteersWithPaginationUseCase> logger)
+    {
+        _readDBContext = readDBContext;
+        _logger = logger;
+    }
+
+    public async Task<Result<IReadOnlyList<VolunteerDto>>> Execute(
+        CancellationToken ct)
+    {
+        var volunteerDtos = _readDBContext.Volunteers.ToList();
+        _logger.LogInformation("Получено {0} волонётров", volunteerDtos.Count);
+        return volunteerDtos;
+    }
 }
