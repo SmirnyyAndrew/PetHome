64f6c2e1dc47dac28a0579c3d3f4fe75afeb8d49
Андрей Смирный <smirnay2001@mail.ru>
2024-12-18 15:00:59 +0300

    Реализовал PagedList

---
+++ src/PetHome.Application/Models/PagedList.cs
@@ -0,0 +1,10 @@
+﻿namespace PetHome.Application.Models;
+public class PagedList<T>
+{
+    public int PageNumber { get; init; }
+    public int PageSize { get; init; }
+    public IReadOnlyList<T> Items { get; init; }
+    public int Count => Items.Count;
+    public bool HasNextPage => PageSize * PageNumber < Count - PageSize; 
+    public bool HasPreviousPage => PageNumber > 1;
+}
