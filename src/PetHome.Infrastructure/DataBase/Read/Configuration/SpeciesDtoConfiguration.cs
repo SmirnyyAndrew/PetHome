7155f1f6ea98c1c6e988fe446ad0e06b89f6ebfa
Андрей Смирный <smirnay2001@mail.ru>
2024-12-20 18:53:14 +0300

    Добавил в конфигурации speciesDto propepty поле id

--- src/PetHome.Infrastructure/DataBase/Read/Configuration/SpeciesDtoConfiguration.cs
+++ src/PetHome.Infrastructure/DataBase/Read/Configuration/SpeciesDtoConfiguration.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PetHome.Domain.PetManagment.PetEntity;
 
 namespace PetHome.Infrastructure.DataBase.Read.Configuration;
 public class SpeciesDtoConfiguration : IEntityTypeConfiguration<SpeciesDto>
@@ -7,6 +8,9 @@ public class SpeciesDtoConfiguration : IEntityTypeConfiguration<SpeciesDto>
     public void Configure(EntityTypeBuilder<SpeciesDto> builder)
     {
         builder.ToTable("specieses");
+        builder.Property(s => s.Id) 
+            .IsRequired()
+            .HasColumnName("Id");
 
         //id
         builder.HasKey(x => x.Id);
