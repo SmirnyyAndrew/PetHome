438e5972fbe62776c56da146acd328174d9d0391
Андрей Смирный <smirnay2001@mail.ru>
2024-12-20 18:31:21 +0300

    1. Разделил контроллеры для Breed и Species 2. Рефакторинг 3. В контроллере поменял command на request в методе CreateSpecies

---
+++ src/PetHome.API/Controllers/PetManegment/PetBreedController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PetHome.API.Controllers.PetManegment.Requests;
+using PetHome.Application.Features.Write.PetManegment.CreateBreed;
+
+namespace PetHome.API.Controllers.PetManegment;
+
+public class PetBreedController : ParentController
+{
+    [HttpPost("breeds")]
+    public async Task<IActionResult> CreateBreed(
+        [FromBody] CreateBreedRequest request,
+        [FromServices] CreateBreedUseCase useCase,
+        CancellationToken ct = default)
+    {
+        var createBreedResult = await useCase.Execute(
+            request,
+            ct);
+        if (createBreedResult.IsFailure)
+            return BadRequest(createBreedResult.Error);
+
+
+        return Ok(createBreedResult.Value);
+    }
+}
---
+++ src/PetHome.API/Controllers/PetManegment/PetSpeciesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PetHome.Application.Features.Write.PetManegment.CreateSpecies;
+
+namespace PetHome.API.Controllers.PetManegment;
+
+public class PetSpeciesController : ParentController
+{
+    [HttpPost("species")]
+    public async Task<IActionResult> CreateSpecies(
+        [FromBody] CreateSpeciesRequest request,
+        [FromServices] CreateSpeciesUseCase useCase,
+        CancellationToken ct = default)
+    {
+        var result = await useCase.Execute(request, ct);
+        if (result.IsFailure)
+            return BadRequest(result.Error);
+
+        return Ok(result.Value);
+    }
+}
