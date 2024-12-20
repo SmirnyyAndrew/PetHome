6383f2584848ed9659e7d5c1cfe71029831a4d31
Андрей Смирный <smirnay2001@mail.ru>
2024-12-20 18:42:15 +0300

    Зарегистрировал use case по получению всех видов животных

--- src/PetHome.Application/Inject.cs
+++ src/PetHome.Application/Inject.cs
@@ -1,6 +1,7 @@
 ﻿using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
 using PetHome.Application.Features.Background;
+using PetHome.Application.Features.Read.PetManegment;
 using PetHome.Application.Features.Read.VolunteerManegment.GetAllVolunteersWithPagination;
 using PetHome.Application.Features.Read.VolunteerManegment.GetVolunteerById;
 using PetHome.Application.Features.Write.PetManegment.ChangeSerialNumber;
@@ -19,6 +20,7 @@ public static class Inject
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
+        //Volunteer manegment
         services.AddScoped<CreateVolunteerUseCase>();
         services.AddScoped<UpdateMainInfoVolunteerUseCase>();
         services.AddScoped<HardDeleteVolunteerUseCase>();
@@ -27,6 +29,7 @@ public static class Inject
         services.AddScoped<GetAllVolunteersWithPaginationUseCase>();
         services.AddScoped<GetVolunteerByIdUseCase>();
 
+        //Pet manegment
         services.AddScoped<SoftDeletedEntitiesToHardDeleteUseCase>();
         services.AddScoped<CreatePetUseCase>();
         services.AddScoped<CreateSpeciesUseCase>();
@@ -35,6 +38,9 @@ public static class Inject
         services.AddScoped<DeletePetMediaFilesUseCase>();
         services.AddScoped<ChangePetSerialNumberUseCase>();
         services.AddValidatorsFromAssembly(typeof(Inject).Assembly);
+
+        //Species manegment
+        services.AddScoped<GetAllSpeciesUseCase>();
         return services;
     } 
 }
