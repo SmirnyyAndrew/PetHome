7e422a84b5df028f596cb02b371aec07f4c7438f
Андрей Смирный <smirnay2001@mail.ru>
2024-12-24 15:24:43 +0300

    Реализовал расширение ToErrorList для Error, для того чтобы избежать приведение типов

---
+++ src/PetHome.Application/Extentions/ErrorExtention.cs
@@ -0,0 +1,11 @@
+﻿using PetHome.Application.Validator;
+using PetHome.Domain.Shared.Error;
+
+namespace PetHome.Application.Extentions;
+public static class ErrorExtention
+{
+    public static ErrorList ToErrorList(this Error error)
+    {
+        return new ErrorList([error]);
+    }
+}
