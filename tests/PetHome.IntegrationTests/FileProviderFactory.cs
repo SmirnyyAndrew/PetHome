551f997e5e1b6e4fd3e7235381c5d92e1c94893e
Андрей Смирный <smirnay2001@mail.ru>
2024-12-25 22:06:16 +0300

    Реализовал factory для работы с IFileProvider (minio)

---
+++ tests/PetHome.IntegrationTests/FileProviderFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using NSubstitute;
+using Xunit;
+
+namespace PetHome.IntegrationTests;
+public class FileProviderFactory : BaseTest, IClassFixture<IntegrationTestFactory>
+{
+    private readonly IFileProvider _fileServiceMock = Substitute.For<IFileProvider>();
+    private static IntegrationTestFactory _factory;
+
+    public FileProviderFactory(IntegrationTestFactory factory) : base(factory)
+    {
+        _factory = factory;
+        _fileServiceMock = factory.Services.GetRequiredService<IFileProvider>();
+        _factory.SetupSuccessFileServiceMock();
+    }
+
+    public static void SetFileServiceSuccess() => _factory.SetupSuccessFileServiceMock();
+    public static void SetFileServiceFailed() => _factory.SetupFailedFileServiceMock();
+}
