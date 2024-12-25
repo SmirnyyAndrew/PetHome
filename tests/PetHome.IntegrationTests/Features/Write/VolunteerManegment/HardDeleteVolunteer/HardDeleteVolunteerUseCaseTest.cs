7533304a9e728b144228014f86ad80403f91fe6d
Андрей Смирный <smirnay2001@mail.ru>
2024-12-25 21:39:34 +0300

    Реализовал тест для HardDeleteVolunteerUseCase

--- tests/PetHome.IntegrationTests/Features/Write/VolunteerManegment/HardDeleteVolunteer/HardDeleteVolunteerUseCaseTest.cs
+++ tests/PetHome.IntegrationTests/Features/Write/VolunteerManegment/HardDeleteVolunteer/HardDeleteVolunteerUseCaseTest.cs
@@ -1,4 +1,31 @@
-﻿namespace PetHome.IntegrationTests.Features.Write.VolunteerManegment.HardDeleteVolunteer;
-public class HardDeleteVolunteerUseCaseTest 
-{ 
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.Extensions.DependencyInjection;
+using PetHome.Application.Features.Write.VolunteerManegment.HardDeleteVolunteer;
+using PetHome.Application.Interfaces.FeatureManagment;
+using Xunit;
+
+namespace PetHome.IntegrationTests.Features.Write.VolunteerManegment.HardDeleteVolunteer;
+public class HardDeleteVolunteerUseCaseTest : BaseTest, IClassFixture<IntegrationTestFactory>
+{
+    private readonly ICommandHandler<bool, HardDeleteVolunteerCommand> _sut;
+    public HardDeleteVolunteerUseCaseTest(IntegrationTestFactory factory) : base(factory)
+    {
+        var scope = factory.Services.CreateScope();
+        _sut = scope.ServiceProvider.GetRequiredService<ICommandHandler<bool, HardDeleteVolunteerCommand>>();
+    }
+
+    [Fact]
+    public async void Success_hard_delete_volunteer()
+    {
+        //array
+        await SeedVolunteers(1);
+        var volunteer = _writeDbContext.Volunteers.First();
+        HardDeleteVolunteerCommand command = new HardDeleteVolunteerCommand(volunteer.Id);
+
+        //act
+        var result = await _sut.Execute(command, CancellationToken.None);
+
+        //assert
+        Assert.True(result.IsSuccess);
+    }
 }
