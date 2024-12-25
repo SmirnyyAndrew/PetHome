58bcb9253457e517943a75b5f7ed7b6b78d6ed52
Андрей Смирный <smirnay2001@mail.ru>
2024-12-25 21:25:55 +0300

    Реализовал тест для SetPetMainPhotoUseCase

--- tests/PetHome.IntegrationTests/Features/Write/PetManegment/SetMainPhoto/SetPetMainPhotoUseCaseTest.cs
+++ tests/PetHome.IntegrationTests/Features/Write/PetManegment/SetMainPhoto/SetPetMainPhotoUseCaseTest.cs
@@ -1,4 +1,34 @@
-﻿namespace PetHome.IntegrationTests.Features.Write.PetManegment.SetMainPhoto;
-public class SetPetMainPhotoUseCaseTest 
-{ 
+﻿using Microsoft.Extensions.DependencyInjection;
+using PetHome.Application.Features.Write.PetManegment.SetMainPhoto;
+using PetHome.Application.Interfaces.FeatureManagment;
+using Xunit;
+
+namespace PetHome.IntegrationTests.Features.Write.PetManegment.SetMainPhoto;
+public class SetPetMainPhotoUseCaseTest : BaseTest, IClassFixture<IntegrationTestFactory>
+{
+    private readonly ICommandHandler<SetPetMainPhotoCommand> _sut;
+    public SetPetMainPhotoUseCaseTest(IntegrationTestFactory factory) : base(factory)
+    {
+        var scope = factory.Services.CreateScope();
+        _sut = scope.ServiceProvider.GetRequiredService<ICommandHandler<SetPetMainPhotoCommand>>();
+    }
+
+    [Fact]
+    public async void Success_set_pet_main_info()
+    {
+        //array
+        await SeedVolunteersWithAggregates();
+        var pet = _writeDbContext.Volunteers.SelectMany(p => p.Pets).First();
+        SetPetMainPhotoCommand command = new SetPetMainPhotoCommand(
+            pet.VolunteerId,
+            pet.Id,
+            "photos",
+            Guid.NewGuid().ToString());
+
+        //act
+        var result = await _sut.Execute(command, CancellationToken.None);
+
+        //assert
+        Assert.True(result.IsSuccess);
+    }
 }
