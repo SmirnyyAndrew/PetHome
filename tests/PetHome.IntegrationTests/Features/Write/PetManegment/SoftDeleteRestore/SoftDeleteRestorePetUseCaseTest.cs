ef056c093b628eabb13da22ca9dda37ae6e60331
Андрей Смирный <smirnay2001@mail.ru>
2024-12-25 21:29:53 +0300

    Реализовал тест для SoftDeleteRestorePetUseCase

--- tests/PetHome.IntegrationTests/Features/Write/PetManegment/SoftDeleteRestore/SoftDeleteRestorePetUseCaseTest.cs
+++ tests/PetHome.IntegrationTests/Features/Write/PetManegment/SoftDeleteRestore/SoftDeleteRestorePetUseCaseTest.cs
@@ -1,4 +1,33 @@
-﻿namespace PetHome.IntegrationTests.Features.Write.PetManegment.SoftDeleteRestore;
-public class SoftDeleteRestorePetUseCaseTest 
-{ 
+﻿using Microsoft.Extensions.DependencyInjection;
+using PetHome.Application.Features.Write.PetManegment.SoftDeleteRestore;
+using PetHome.Application.Interfaces.FeatureManagment;
+using Xunit;
+
+namespace PetHome.IntegrationTests.Features.Write.PetManegment.SoftDeleteRestore;
+public class SoftDeleteRestorePetUseCaseTest : BaseTest, IClassFixture<IntegrationTestFactory>
+{
+    private readonly ICommandHandler<SoftDeleteRestorePetCommand> _sut;
+    public SoftDeleteRestorePetUseCaseTest(IntegrationTestFactory factory) : base(factory)
+    {
+        var scope = factory.Services.CreateScope();
+        _sut = scope.ServiceProvider.GetRequiredService<ICommandHandler<SoftDeleteRestorePetCommand>>();
+    }
+
+    [Fact]
+    public async void Success_soft_delete_restore()
+    {
+        //array
+        await SeedVolunteersWithAggregates();
+        var pet = _writeDbContext.Volunteers.SelectMany(p => p.Pets).First();
+        SoftDeleteRestorePetCommand command = new SoftDeleteRestorePetCommand(
+            pet.VolunteerId,
+            pet.Id,
+            true);
+
+        //act
+        var result = await _sut.Execute(command, CancellationToken.None);
+
+        //assert
+        Assert.True(result.IsSuccess);
+    }
 }
