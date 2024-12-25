f0cf55ab186f1ee056d04d531741788ece45de64
Андрей Смирный <smirnay2001@mail.ru>
2024-12-25 21:17:41 +0300

    Реализовал тест для DeleteSpeciesByIdUseCase

--- tests/PetHome.IntegrationTests/Features/Write/PetManegment/DeleteSpeciesById/DeleteSpeciesByIdUseCaseTest.cs
+++ tests/PetHome.IntegrationTests/Features/Write/PetManegment/DeleteSpeciesById/DeleteSpeciesByIdUseCaseTest.cs
@@ -1,4 +1,30 @@
-﻿namespace PetHome.IntegrationTests.Features.Write.PetManegment.DeleteSpeciesById;
-public class DeleteSpeciesByIdUseCaseTest 
-{ 
+﻿using Microsoft.Extensions.DependencyInjection;
+using PetHome.Application.Features.Write.PetManegment.DeleteSpeciesById;
+using PetHome.Application.Interfaces.FeatureManagment;
+using Xunit;
+
+namespace PetHome.IntegrationTests.Features.Write.PetManegment.DeleteSpeciesById;
+public class DeleteSpeciesByIdUseCaseTest : BaseTest, IClassFixture<IntegrationTestFactory>
+{
+    private readonly ICommandHandler<string, DeleteSpeciesByIdCommand> _sut;
+    public DeleteSpeciesByIdUseCaseTest(IntegrationTestFactory factory) : base(factory)
+    {
+        var scope = factory.Services.CreateScope();
+        _sut = scope.ServiceProvider.GetRequiredService<ICommandHandler<string, DeleteSpeciesByIdCommand>>();
+    }
+
+    [Fact]
+    public async void Success_delete_species_by_id()
+    {
+        //array
+        await SeedSpecies(1);
+        var species = _writeDbContext.Species.First();
+        DeleteSpeciesByIdCommand command = new DeleteSpeciesByIdCommand(species.Id);
+
+        //act
+        var result = await _sut.Execute(command, CancellationToken.None);
+
+        //assert
+        Assert.True(result.IsSuccess);
+    }
 }
