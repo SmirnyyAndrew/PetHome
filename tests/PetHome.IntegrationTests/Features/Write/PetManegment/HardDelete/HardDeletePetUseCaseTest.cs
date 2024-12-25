2df48fa15e571422674edc385b817b4a60893764
Андрей Смирный <smirnay2001@mail.ru>
2024-12-25 21:21:36 +0300

    Реализовал тест для HardDeletePetUseCase

--- tests/PetHome.IntegrationTests/Features/Write/PetManegment/HardDelete/HardDeletePetUseCaseTest.cs
+++ tests/PetHome.IntegrationTests/Features/Write/PetManegment/HardDelete/HardDeletePetUseCaseTest.cs
@@ -1,4 +1,31 @@
-﻿namespace PetHome.IntegrationTests.Features.Write.PetManegment.HardDelete;
-public class HardDeletePetUseCaseTest  
-{ 
+﻿using Microsoft.Extensions.DependencyInjection;
+using PetHome.Application.Features.Write.PetManegment.DeleteSpeciesById;
+using PetHome.Application.Features.Write.PetManegment.HardDelete;
+using PetHome.Application.Interfaces.FeatureManagment;
+using Xunit;
+
+namespace PetHome.IntegrationTests.Features.Write.PetManegment.HardDelete;
+public class HardDeletePetUseCaseTest : BaseTest, IClassFixture<IntegrationTestFactory>
+{
+    private readonly ICommandHandler<HardDeletePetCommand> _sut;
+    public HardDeletePetUseCaseTest(IntegrationTestFactory factory) : base(factory)
+    {
+        var scope = factory.Services.CreateScope();
+        _sut = scope.ServiceProvider.GetRequiredService<ICommandHandler<HardDeletePetCommand>>();
+    }
+
+    [Fact]
+    public async void Success_delete_species_by_id()
+    {
+        //array
+        await SeedVolunteersWithAggregates();
+        var pet = _writeDbContext.Volunteers.SelectMany(p => p.Pets).First();
+        HardDeletePetCommand command = new HardDeletePetCommand(pet.VolunteerId, pet.Id);
+
+        //act
+        var result = await _sut.Execute(command, CancellationToken.None);
+
+        //assert
+        Assert.True(result.IsSuccess);
+    }
 }
