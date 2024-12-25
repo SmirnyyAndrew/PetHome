2b04217d8eeb8a72833fd2538f92701c6cc4302b
Андрей Смирный <smirnay2001@mail.ru>
2024-12-25 21:13:36 +0300

    Реализовал тест для CreateSpeciesUseCase

--- tests/PetHome.IntegrationTests/Features/Write/PetManegment/CreateSpecies/CreateSpeciesUseCaseTest.cs
+++ tests/PetHome.IntegrationTests/Features/Write/PetManegment/CreateSpecies/CreateSpeciesUseCaseTest.cs
@@ -1,4 +1,28 @@
-﻿namespace PetHome.IntegrationTests.Features.Write.PetManegment.CreateSpecies;
-public class CreateSpeciesUseCaseTest  
-{ 
+﻿using Microsoft.Extensions.DependencyInjection;
+using PetHome.Application.Features.Write.PetManegment.CreateSpecies;
+using PetHome.Application.Interfaces.FeatureManagment;
+using Xunit;
+
+namespace PetHome.IntegrationTests.Features.Write.PetManegment.CreateSpecies;
+public class CreateSpeciesUseCaseTest : BaseTest, IClassFixture<IntegrationTestFactory>
+{
+    private readonly ICommandHandler<Guid, CreateSpeciesCommand> _sut;
+    public CreateSpeciesUseCaseTest(IntegrationTestFactory factory) : base(factory)
+    {
+        var scope = factory.Services.CreateScope();
+        _sut = scope.ServiceProvider.GetRequiredService<ICommandHandler<Guid, CreateSpeciesCommand>>();
+    }
+
+    [Fact]
+    public async void Success_create_species()
+    {
+        //array
+        CreateSpeciesCommand command = new CreateSpeciesCommand("Собака");
+
+        //act
+        var result = await _sut.Execute(command, CancellationToken.None);
+
+        //assert
+        Assert.True(result.IsSuccess);
+    }
 }
