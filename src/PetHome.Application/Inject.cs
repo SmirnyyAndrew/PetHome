a144c029a52f5d242435906a42a4403ec90c22de
Андрей Смирный <smirnay2001@mail.ru>
2024-12-26 18:02:50 +0300

    Рефакторинг регистрации сервисов (не работают сервысы IQueryHandler<,>, у которых в возвращаемом типе есть PagedList<T>

--- src/PetHome.Application/Inject.cs
+++ src/PetHome.Application/Inject.cs
@@ -2,51 +2,24 @@
 using Microsoft.Extensions.DependencyInjection;
 using PetHome.Application.Features.Background;
 using PetHome.Application.Interfaces.FeatureManagment;
+using PetHome.Application.Models;
 
 namespace PetHome.Application;
 public static class Inject
 {
+
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.Scan(scan => scan.FromAssemblies(typeof(Inject).Assembly)
         .AddClasses(classes => classes
-            .AssignableToAny(typeof(ICommandHandler<,>), typeof(ICommandHandler<>))
-            .AssignableToAny(typeof(IQueryHandler<,>), typeof(IQueryHandler<>)))
+            .AssignableToAny(
+                typeof(ICommandHandler<>), typeof(ICommandHandler<,>),
+                typeof(IQueryHandler<>), typeof(IQueryHandler<,>)))
         .AsSelfWithInterfaces()
         .WithScopedLifetime());
-
+       
         services.AddValidatorsFromAssembly(typeof(Inject).Assembly);
 
-        /*
-        //Volunteer manegment
-        services.AddScoped<CreateVolunteerUseCase>();
-        services.AddScoped<UpdateMainInfoVolunteerUseCase>();
-        services.AddScoped<HardDeleteVolunteerUseCase>();
-        services.AddScoped<SoftDeleteVolunteerUseCase>();
-        services.AddScoped<SoftRestoreVolunteerUseCase>();
-        services.AddScoped<GetAllVolunteersWithPaginationUseCase>();
-        services.AddScoped<GetVolunteerByIdUseCase>();
-
-        //Pet manegment
-        services.AddScoped<SoftDeletedEntitiesToHardDeleteUseCase>();
-        services.AddScoped<CreatePetUseCase>();
-        services.AddScoped<CreateSpeciesUseCase>();
-        services.AddScoped<CreateBreedUseCase>();
-        services.AddScoped<UploadPetMediaFilesUseCase>();
-        services.AddScoped<DeletePetMediaFilesUseCase>();
-        services.AddScoped<ChangePetSerialNumberUseCase>();
-        services.AddScoped<ChangePetInfoUseCase>();
-        services.AddScoped<ChangePetStatusUseCase>();
-        services.AddScoped<HardDeletePetUseCase>();
-        services.AddScoped<SoftDeleteRestorePetUseCase>();
-        services.AddScoped<SetPetMainPhotoUseCase>();
-
-        //Species manegment
-        services.AddScoped<GetAllSpeciesUseCase>();
-
-        //Breed manegment
-        services.AddScoped<GetAllBreedDtoBySpeciesIdUseCase>();
-        services.AddScoped<DeleteSpeciesByIdUseCase>();*/
         return services;
     }
 }
