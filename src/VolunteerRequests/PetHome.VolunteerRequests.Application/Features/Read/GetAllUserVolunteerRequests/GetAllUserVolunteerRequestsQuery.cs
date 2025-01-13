fd5ee2a1d178b960eaf19963372edd096d84bd31
Андрей Смирный <smirnay2001@mail.ru>
2025-01-14 00:05:33 +0300

    Реализовал GetAllUserVolunteerRequests

---
+++ src/VolunteerRequests/PetHome.VolunteerRequests.Application/Features/Read/GetAllUserVolunteerRequests/GetAllUserVolunteerRequestsQuery.cs
@@ -0,0 +1,4 @@
+﻿using PetHome.Core.Interfaces.FeatureManagment;
+
+namespace PetHome.VolunteerRequests.Application.Features.Read.GetAllUserVolunteerRequests;
+public record GetAllUserVolunteerRequestsQuery(Guid UserId) : IQuery;
---
+++ src/VolunteerRequests/PetHome.VolunteerRequests.Application/Features/Read/GetAllUserVolunteerRequests/GetAllUserVolunteerRequestsUseCase.cs
@@ -0,0 +1,28 @@
+﻿using CSharpFunctionalExtensions;
+using Microsoft.EntityFrameworkCore;
+using PetHome.Core.Interfaces.FeatureManagment;
+using PetHome.Core.Response.Validation.Validator;
+using PetHome.VolunteerRequests.Application.Database.Dto;
+using PetHome.VolunteerRequests.Application.Database.Interfaces;
+
+namespace PetHome.VolunteerRequests.Application.Features.Read.GetAllUserVolunteerRequests;
+public class GetAllUserVolunteerRequestsUseCase
+    : IQueryHandler<IReadOnlyList<VolunteerRequestDto>, GetAllUserVolunteerRequestsQuery>
+{
+    private readonly IVolunteerRequestReadDbContext _readDbContext;
+
+    public GetAllUserVolunteerRequestsUseCase(
+        IVolunteerRequestReadDbContext readDbContext)
+    {
+        _readDbContext = readDbContext;
+    }
+    public async Task<Result<IReadOnlyList<VolunteerRequestDto>, ErrorList>> Execute(
+        GetAllUserVolunteerRequestsQuery query, CancellationToken ct)
+    {
+        IReadOnlyList<VolunteerRequestDto>? volunteerRequests = await _readDbContext.VolunteerRequests
+            .Where(r => r.UserId == query.UserId)
+            .ToListAsync(ct);
+
+        return volunteerRequests.ToList();
+    }
+}
