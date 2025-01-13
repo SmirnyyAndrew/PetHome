bdccc05596aa3876efc2725b29d50ea1901c7116
Андрей Смирный <smirnay2001@mail.ru>
2025-01-14 00:00:39 +0300

    Реализовал GetAllAdminVolunteerRequests

---
+++ src/VolunteerRequests/PetHome.VolunteerRequests.Application/Features/Read/GetAllAdminVolunteerRequests/GetAllAdminVolunteerRequestsQuery.cs
@@ -0,0 +1,4 @@
+﻿using PetHome.Core.Interfaces.FeatureManagment;
+
+namespace PetHome.VolunteerRequests.Application.Features.Read.GetAllAdminVolunteerRequests;
+public record GetAllAdminVolunteerRequestsQuery(Guid AdminId):IQuery;
---
+++ src/VolunteerRequests/PetHome.VolunteerRequests.Application/Features/Read/GetAllAdminVolunteerRequests/GetAllAdminVolunteerRequestsUseCase.cs
@@ -0,0 +1,26 @@
+﻿using CSharpFunctionalExtensions;
+using PetHome.Core.Interfaces.FeatureManagment;
+using PetHome.Core.Response.Validation.Validator;
+using PetHome.VolunteerRequests.Application.Database.Interfaces;
+using PetHome.VolunteerRequests.Domain;
+
+namespace PetHome.VolunteerRequests.Application.Features.Read.GetAllAdminVolunteerRequests;
+public class GetAllAdminVolunteerRequestsUseCase
+    : IQueryHandler<IReadOnlyList<VolunteerRequest>, GetAllAdminVolunteerRequestsQuery>
+{
+    private readonly IVolunteerRequestRepository _repository;
+
+    public GetAllAdminVolunteerRequestsUseCase(
+        IVolunteerRequestRepository repository)
+    {
+        _repository = repository;
+    }
+
+
+    public async Task<Result<IReadOnlyList<VolunteerRequest>, ErrorList>> Execute(
+        GetAllAdminVolunteerRequestsQuery query, CancellationToken ct)
+    {
+        IReadOnlyList<VolunteerRequest> volunteerRequests = await _repository.GetByAdminId(query.AdminId, ct);
+        return volunteerRequests.ToList();
+    }
+}
