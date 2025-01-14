fa2dea1e686db419f27a9292ab9f3cdfbdac61da
Андрей Смирный <smirnay2001@mail.ru>
2025-01-14 11:46:58 +0300

    Реализовал для модуля Discussion хэндлер GetAllDiscussionByRelationIdUseCase (Read)

---
+++ src/Discussions/PetHome.Discussions.Application/Features/Read/GetAllDiscussionByRelationId/GetAllDiscussionByRelationIdUseCase.cs
@@ -0,0 +1,34 @@
+﻿using CSharpFunctionalExtensions;
+using Microsoft.EntityFrameworkCore;
+using PetHome.Core.Extentions.Collection;
+using PetHome.Core.Interfaces.FeatureManagment;
+using PetHome.Core.Models;
+using PetHome.Core.Response.Validation.Validator;
+using PetHome.Discussions.Application.Database.Dto;
+using PetHome.Discussions.Application.Database.Interfaces;
+
+namespace PetHome.Discussions.Application.Features.Read.GetAllDiscussionByRelationId;
+public class GetAllDiscussionByRelationIdUseCase
+    : IQueryHandler<PagedList<DiscussionDto>, GetAllDiscussionByRelationIdQuery>
+{
+    private readonly IDiscussionReadDbContext _readDbContext;
+
+    public GetAllDiscussionByRelationIdUseCase(IDiscussionReadDbContext readDbContext)
+    {
+        _readDbContext = readDbContext;
+    }
+
+    public async Task<Result<PagedList<DiscussionDto>, ErrorList>> Execute(
+        GetAllDiscussionByRelationIdQuery query, CancellationToken ct)
+    {
+        List<DiscussionDto> discussions = await _readDbContext.Discussions
+            .Include(d => d.Messages)
+            .Where(d => d.RelationId == query.RelationId)
+            .ToListAsync(ct);
+
+        var discussionsPagedList = await discussions.AsQueryable()
+            .ToPagedList(query.PageNum, query.PageSize, ct);
+
+        return discussionsPagedList;
+    }
+}
