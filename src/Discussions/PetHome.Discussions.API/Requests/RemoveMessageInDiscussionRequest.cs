5d414dcc7bc7fdd8ec12b0a7e923d30be0d335b6
Андрей Смирный <smirnay2001@mail.ru>
2025-01-14 13:18:50 +0300

    Реализовал Request'ы для слоя API у Discussion

---
+++ src/Discussions/PetHome.Discussions.API/Requests/RemoveMessageInDiscussionRequest.cs
@@ -0,0 +1,10 @@
+﻿using PetHome.Discussions.Application.Features.Write.RemoveMessageInDiscussion;
+
+namespace PetHome.Discussions.API.Requests;
+public record RemoveMessageInDiscussionRequest(Guid DiscussionId, Guid UserId, Guid MessageId)
+{ 
+    public static implicit operator RemoveMessageInDiscussionCommand(RemoveMessageInDiscussionRequest request)
+    {
+        return new RemoveMessageInDiscussionCommand(request.DiscussionId, request.UserId, request.MessageId);
+    }
+}
---
+++ src/Discussions/PetHome.Discussions.API/Requests/SendMessageInDiscussionRequest.cs
@@ -0,0 +1,10 @@
+﻿using PetHome.Discussions.Application.Features.Write.SendMessageInDiscussion;
+
+namespace PetHome.Discussions.API.Requests;
+public record SendMessageInDiscussionRequest(Guid DiscussionId, Guid UserId, string Message)
+{ 
+    public static implicit operator SendMessageInDiscussionCommand(SendMessageInDiscussionRequest request)
+    {
+        return new SendMessageInDiscussionCommand(request.DiscussionId, request.UserId, request.Message);
+    }
+}
