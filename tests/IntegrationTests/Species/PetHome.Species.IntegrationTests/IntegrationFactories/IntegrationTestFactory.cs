e7c8b6884be7e75acd170f0e0bb3fa66dad4b248
Андрей Смирный <smirnay2001@mail.ru>
2025-01-24 12:03:40 +0300

    Вынес интеграционные тесты у Species в отдельный проект

---
+++ tests/IntegrationTests/Species/PetHome.Species.IntegrationTests/IntegrationFactories/IntegrationTestFactory.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Npgsql;
+using PetHome.Species.Application.Database;
+using PetHome.Species.Infrastructure.Database.Read.DBContext;
+using PetHome.Species.Infrastructure.Database.Write.DBContext;
+using Respawn;
+using System.Data.Common;
+using Testcontainers.PostgreSql;
+using Xunit;
+
+namespace PetHome.Species.IntegrationTests.IntegrationFactories;
+
+public class IntegrationTestFactory
+    : WebApplicationFactory<PetHome.API.Program>, IAsyncLifetime
+{
+    protected readonly PostgreSqlContainer _dbContainer = new PostgreSqlBuilder()
+        .WithImage("postgres")
+        .WithDatabase("pet_home_test")
+        .WithUsername("postgres")
+        .WithPassword("postgres")
+        .Build();
+
+    private Respawner _respawner;
+    private DbConnection _dbConnection; 
+    private SpeciesWriteDbContext _writeDbContext;
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.UseEnvironment("SpeciesTesting");
+        builder.ConfigureTestServices(ConfigureDefault);
+    }
+
+    private void ConfigureDefault(IServiceCollection services)
+    {
+        services.RemoveAll(typeof(ISpeciesReadDbContext));  
+        services.RemoveAll(typeof(SpeciesWriteDbContext)); 
+
+          
+        services.AddScoped(_ =>
+               new SpeciesWriteDbContext(_dbContainer.GetConnectionString()));  
+        services.AddScoped<ISpeciesReadDbContext, SpeciesReadDbContext>(_ =>
+              new SpeciesReadDbContext(_dbContainer.GetConnectionString())); 
+    }
+
+    public async Task InitializeAsync()
+    {
+        await _dbContainer.StartAsync();
+
+        _dbConnection = new NpgsqlConnection(_dbContainer.GetConnectionString());
+        _writeDbContext = Services.CreateScope().ServiceProvider.GetRequiredService<SpeciesWriteDbContext>();
+
+        await _writeDbContext.Database.EnsureCreatedAsync();
+
+        await InilizeRespawner();
+    }
+
+    async Task IAsyncLifetime.DisposeAsync()
+    {
+        await _dbContainer.StopAsync();
+        await _dbContainer.DisposeAsync();
+        await ResetDatabaseAsync();
+    }
+
+    public async Task ResetDatabaseAsync()
+    {
+        if (_respawner is not null)
+            await _respawner.ResetAsync(_dbConnection);
+    }
+
+    private async Task InilizeRespawner()
+    {
+        await _dbConnection.OpenAsync();
+
+        RespawnerOptions respawnerOptions = new RespawnerOptions()
+        {
+            DbAdapter = DbAdapter.Postgres,
+            SchemasToInclude = ["public"]
+        };
+        _respawner = await Respawner.CreateAsync(
+            _dbConnection,
+            respawnerOptions);
+    } 
+}
---
+++ tests/IntegrationTests/Species/PetHome.Species.IntegrationTests/Seeds/SeedManager.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PetHome.Species.Infrastructure.Database.Write.DBContext;
+using PetHome.Species.IntegrationTests.IntegrationFactories;
+
+namespace PetHome.Species.IntegrationTests.Seeds.Species;
+public partial class SeedManager
+{
+    protected readonly SpeciesWriteDbContext _writeDbContext; 
+    public SeedManager(IntegrationTestFactory factory)
+    {
+        var scope = factory.Services.CreateScope();
+        _writeDbContext = scope.ServiceProvider.GetRequiredService<SpeciesWriteDbContext>();
+    }
+}
