3bb28aacceaed17a81b3bb83576ad9c747f8c16d
Андрей Смирный <smirnay2001@mail.ru>
2025-01-24 12:23:11 +0300

    Создал проект для интеграционных тестов у модуля VolunteerRequest

---
+++ tests/IntegrationTests/VolunteerRequests/PetHome.VolunteerRequests.IntegrationTests/Seeds/SeedManager.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PetHome.VolunteerRequests.Infrastructure.Database;
+using PetHome.VolunteerRequests.IntegrationTests.IntegrationFactories;
+
+namespace PetHome.VolunteerRequests.IntegrationTests.Seeds;
+public partial class SeedManager
+{
+    protected readonly VolunteerRequestDbContext _writeDbContext; 
+    public SeedManager(IntegrationTestFactory factory)
+    {
+        var scope = factory.Services.CreateScope();
+        _writeDbContext = scope.ServiceProvider.GetRequiredService<VolunteerRequestDbContext>();
+    }
+}
