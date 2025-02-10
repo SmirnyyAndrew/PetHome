892fe1790fd2675538a94882d0f93ab41948266e
Андрей Смирный <smirnay2001@mail.ru>
2025-02-10 15:39:29 +0300

    Реализовал фичу загрузки части файла

---
+++ FilesService/Features/UploadPresignedPartUrl.cs
@@ -0,0 +1,55 @@
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+using FilesService.Endpoints;
+
+namespace FilesService.Features;
+
+public static class UploadPresignedPartUrl
+{
+    private record UploadPresignedPartUrlRequest(
+       string BucketName,
+       string UploadId,
+       int PartNumber);
+
+    public sealed class Endpoint : IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            app.MapPost("files/part-presigned", Handler);
+        }
+    }
+    private static async Task<IResult> Handler(
+           UploadPresignedPartUrlRequest request,
+           IAmazonS3 s3Client,
+           CancellationToken ct)
+    {
+        try
+        {
+            Guid key = Guid.NewGuid();
+
+            GetPreSignedUrlRequest presignedRequest = new GetPreSignedUrlRequest
+            {
+                BucketName = request.BucketName,
+                Key = key.ToString(),
+                Verb = HttpVerb.PUT,
+                Expires = DateTime.UtcNow.AddDays(14),
+                //ContentType = request.ContentType,
+                Protocol = Protocol.HTTP, 
+                UploadId = request.UploadId,
+                PartNumber = request.PartNumber
+            };
+
+            string? presignedUrl = await s3Client.GetPreSignedURLAsync(presignedRequest);
+
+            return Results.Ok(new
+            {
+                key,
+                presignedUrl
+            });
+        }
+        catch (Exception ex)
+        {
+            return Results.BadRequest($"S3: part upload presigned url failed: \r\t\n{ex}");
+        }
+    }
+}
