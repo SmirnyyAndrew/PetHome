970c97d4e36a13f70fe64eba2f34038277e8ad6e
Андрей Смирный <smirnay2001@mail.ru>
2025-02-11 01:56:42 +0300

    Реализовал фичу по получению коллекции FileData по Id

---
+++ FilesService/src/FilesService/Application/Features/AmazonS3/GetFilesDataByIds.cs
@@ -0,0 +1,28 @@
+﻿using FilesService.Application.Endpoints;
+using FilesService.Infrastructure.MongoDB;
+
+namespace FilesService.Application.Features.AmazonS3;
+
+public static class GetFilesDataByIds
+{
+    private record GetFilesDataByIdsRequest(Guid[] Ids);
+
+    public sealed class Endpoint : IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            app.MapGet("files", Handler);
+        }
+    }
+    private static async Task<IResult> Handler(
+           GetFilesDataByIdsRequest request,
+           MongoDbRepository repository,
+           CancellationToken ct)
+    {
+        var result = await repository.Get(request.Ids, ct);
+        if (result.IsSuccess)
+            return Results.Ok(result.Value);
+
+        return Results.BadRequest();
+    }
+}
