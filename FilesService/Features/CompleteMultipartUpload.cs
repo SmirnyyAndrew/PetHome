c8cecbeccd9ca6adb35791e26127c012d16b1661
Андрей Смирный <smirnay2001@mail.ru>
2025-02-10 15:39:46 +0300

    Реализовал фичу завершения multipart upload

---
+++ FilesService/Features/CompleteMultipartUpload.cs
@@ -0,0 +1,54 @@
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+using FilesService.Endpoints;
+
+namespace FilesService.Features;
+
+public static class CompleteMultipartUpload
+{
+    public record PartETagInfo(int PartNumber, string ETag);
+     
+    private record CompleteMultipartRequest(
+       string BucketName,
+       string UploadId,
+       List<PartETagInfo> Parts);
+
+    public sealed class Endpoint : IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            app.MapPost("files/{key:guid}/complite-multipart/presigned", Handler);
+        }
+    }
+    private static async Task<IResult> Handler(
+           Guid key,
+           CompleteMultipartRequest request,
+           IAmazonS3 s3Client,
+           CancellationToken ct)
+    {
+        try
+        {
+            var presignedRequest = new CompleteMultipartUploadRequest
+            {
+                BucketName = request.BucketName,
+                Key = key.ToString(),
+                UploadId = request.UploadId,
+                PartETags = request.Parts
+                    .Select(p => new PartETag(p.PartNumber, p.ETag)).ToList()
+            };
+
+            var response = await s3Client.CompleteMultipartUploadAsync(
+                presignedRequest, ct);
+
+            return Results.Ok(new
+            {
+                key,
+                response.Location
+            });
+        }
+        catch (Exception ex)
+        {
+            return Results.BadRequest($"S3: complete multipart upload failed: \r\t\n{ex}");
+        }
+    }
+}
