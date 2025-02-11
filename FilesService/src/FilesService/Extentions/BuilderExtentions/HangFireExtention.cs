1631ed7a26006063ad9d54e75a9c36f0cb8be090
Андрей Смирный <smirnay2001@mail.ru>
2025-02-11 17:27:14 +0300

    Подключил Hangfire

---
+++ FilesService/src/FilesService/Extentions/BuilderExtentions/HangFireExtention.cs
@@ -0,0 +1,22 @@
+﻿using Hangfire;
+using Hangfire.PostgreSql;
+
+namespace FilesService.Extentions.BuilderExtentions;
+
+public static class HangFireExtention
+{
+    public static IServiceCollection AddHangFire(
+        this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddHangfire(config => config
+        .SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
+        .UseSimpleAssemblyNameTypeSerializer()
+        .UseRecommendedSerializerSettings()
+        .UsePostgreSqlStorage(configuration.GetConnectionString("hangfire")));
+
+        // Add the processing server as IHostedService
+        services.AddHangfireServer();
+
+        return services;
+    }
+}
---
+++ FilesService/src/FilesService/Extentions/BuilderExtentions/MinioExtentions.cs
@@ -0,0 +1,22 @@
+﻿using FilesService.Core.Options;
+using Minio;
+
+namespace FilesService.Extentions.BuilderExtentions;
+
+public static class MinioExtentions
+{
+    public static IServiceCollection SetMinioOptions(
+        this IServiceCollection services, IConfiguration configuration)
+    {
+        var minioOptions = configuration.GetSection(MinioOptions.MINIO_NAME).Get<MinioOptions>()
+            ?? throw new Exception("Ошибка со строкой подключения minio. Проверьте конфигурацию.");
+
+        services.AddMinio(options =>
+        {
+            options.WithEndpoint(minioOptions.Endpoint);
+            options.WithCredentials(minioOptions.Accesskey, minioOptions.Secretkey);
+            options.WithSSL(minioOptions.IsWithSSL);
+        });
+        return services;
+    }
+}
