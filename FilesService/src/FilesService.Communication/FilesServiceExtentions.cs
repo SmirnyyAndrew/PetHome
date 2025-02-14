d695354d34dcdecf78deaadbd168a08f16527dca
Андрей Смирный <smirnay2001@mail.ru>
2025-02-14 18:47:22 +0300

    Добавил IOptions для FilesService

---
+++ FilesService/src/FilesService.Communication/FilesServiceExtentions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace FilesService.Communication;
+public static class FilesServiceExtentions
+{
+    public static IServiceCollection AddFileHttpCommunication(
+        this IServiceCollection services, string sectionName, IConfiguration configuration)
+    {
+        services.Configure<FilesServiceOptions>(configuration.GetSection(FilesServiceOptions.SECTION_NAME));
+
+        services.AddHttpClient<FilesHttpClient>((sp, config) =>
+        {
+            var filesServiceOptions = sp.GetRequiredService<IOptions<FilesServiceOptions>>().Value;
+            config.BaseAddress = new Uri(filesServiceOptions.Url);
+        });
+
+        return services;
+    }
+}
