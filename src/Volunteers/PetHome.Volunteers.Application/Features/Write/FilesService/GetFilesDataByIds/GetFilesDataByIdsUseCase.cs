e9da0262b9c5f6d4577bbfc2ac9dd3b1f2863d57
Андрей Смирный <smirnay2001@mail.ru>
2025-02-14 20:56:02 +0300

    Тест files service

---
+++ src/Volunteers/PetHome.Volunteers.Application/Features/Write/FilesService/GetFilesDataByIds/GetFilesDataByIdsUseCase.cs
@@ -0,0 +1,31 @@
+﻿using CSharpFunctionalExtensions;
+using FilesService.Communication;
+using FilesService.Core.Models;
+using PetHome.Core.Extentions.ErrorExtentions;
+using PetHome.Core.Interfaces.FeatureManagment;
+using PetHome.Core.Response.ErrorManagment;
+using PetHome.Core.Response.Validation.Validator;
+
+namespace PetHome.Volunteers.Application.Features.Write.FilesService.GetFilesDataByIds;
+public class GetFilesDataByIdsUseCase
+    : ICommandHandler<IReadOnlyList<FileData>, GetFilesDataByIdsCommand>
+{
+    private readonly FilesHttpClient _httpClient;
+
+    public GetFilesDataByIdsUseCase(FilesHttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<Result<IReadOnlyList<FileData>, ErrorList>> Execute(
+        GetFilesDataByIdsCommand command, CancellationToken ct)
+    {
+        var result = await _httpClient.GetFilesDataByIds(command.Ids, ct);
+        if (result.IsFailure)
+        { 
+            return Errors.NotFound("files").ToErrorList();
+        }
+
+        return result.Value.ToList();
+    }
+}
