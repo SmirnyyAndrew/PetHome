e3b515fb0e427e8293dc51e714c021872ce77dd8
Андрей Смирный <smirnay2001@mail.ru>
2025-02-14 17:38:06 +0300

    Реализовал взаимодействие по http для GetFilesDataByIds

--- FilesService/src/FilesService.Communication/FilesHttpClient.cs
+++ FilesService/src/FilesService.Communication/FilesHttpClient.cs
@@ -1,15 +1,25 @@
 ﻿using CSharpFunctionalExtensions;
-using FilesService.Core.Response;
+using FilesService.Core.ErrorManagment;
+using FilesService.Core.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http.Json;
 
 namespace FilesService.Communication;
 
 public class FilesHttpClient(HttpClient httpClient)
 {
-    public async Task<Result<IReadOnlyList<FileResponse>>> GetFilesPresignedUrls(
-        GetFilesPresignedUrlsRequest request)
+    public async Task<Result<IReadOnlyList<FileData>?, Error>> GetFilesDataByIds(
+        IEnumerable<Guid> ids, CancellationToken ct)
     {
-        return default;
-    }
-}
+        var response = await httpClient.PostAsJsonAsync("files", ids, ct);
 
-public record GetFilesPresignedUrlsRequest();
+        if (response.StatusCode != HttpStatusCode.OK)
+        {
+            return Errors.NotFound("files");
+        }
+        IReadOnlyList<FileData>? files = await response.Content.ReadFromJsonAsync<IReadOnlyList<FileData>>(ct); 
+        return files?.ToList() ?? [];
+    }
+} 
