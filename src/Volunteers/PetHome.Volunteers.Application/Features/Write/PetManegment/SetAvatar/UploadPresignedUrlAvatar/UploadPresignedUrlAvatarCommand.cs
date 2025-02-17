df2a18eeca958ba5df20ddc87f8857f8902e98ac
Андрей Смирный <smirnay2001@mail.ru>
2025-02-18 00:04:12 +0300

    Реализовал установку аватарки для pet

---
+++ src/Volunteers/PetHome.Volunteers.Application/Features/Write/PetManegment/SetAvatar/UploadPresignedUrlAvatar/UploadPresignedUrlAvatarCommand.cs
@@ -0,0 +1,5 @@
+﻿using FilesService.Core.Request.AmazonS3;
+using PetHome.Core.Interfaces.FeatureManagment;
+
+namespace PetHome.Volunteers.Application.Features.Write.PetManegment.SetAvatar.UploadPresignedUrlAvatar;
+public record UploadPresignedUrlAvatarCommand(UploadPresignedUrlRequest UploadPresignedUrlRequest) : ICommand;
