3aebf87d193c36ef706b5e39898702cb9c072f48
Андрей Смирный <smirnay2001@mail.ru>
2025-02-20 18:05:48 +0300

    Реализовал UserScopedData

---
+++ src/Shared/PetHome.Framework/Auth/UserScopedData.cs
@@ -0,0 +1,9 @@
+﻿namespace PetHome.Framework.Auth;
+public class UserScopedData
+{
+    public Guid UserId { get; set; } = Guid.Empty;
+    public Guid RoleId { get; set; } = Guid.Empty;
+    public IReadOnlyList<Guid> PermissionsIds { get; set; } = [];
+    public string UserName { get; set; } = string.Empty;
+}
+ 
