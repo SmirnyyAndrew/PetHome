a4d9da9384d84c9b02d2246160c54cf6a40e7fba
Андрей Смирный <smirnay2001@mail.ru>
2025-02-27 18:32:57 +0300

    Реализовал transaction outbox

---
+++ src/Accounts/PetHome.Accounts.Domain/Others/OutboxMessage.cs
@@ -0,0 +1,10 @@
+﻿namespace PetHome.Accounts.Domain.Others;
+public class OutboxMessage
+{
+    public Guid Id { get; set; }
+    public required string Type { get; set; }
+    public required string Payload { get; set; }
+    public required DateTime OccurredOn { get; set; }
+    public DateTime? ProcessedOn { get; set; }
+    public string? Error { get; set; }
+}
---
+++ src/Accounts/PetHome.Accounts.Infrastructure/Database/Configuration/Outbox/OutboxMessageConfiguration.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PetHome.Accounts.Domain.Others;
+
+namespace PetHome.Accounts.Infrastructure.Database.Configuration.Outbox;
+public class OutboxMessageConfiguration : IEntityTypeConfiguration<OutboxMessage>
+{
+    public void Configure(EntityTypeBuilder<OutboxMessage> builder)
+    {
+        builder.ToTable("outbox_messages");
+
+        builder.HasKey(o => o.Id);
+
+        builder.Property(o => o.Type)
+            .IsRequired();
+
+        builder.Property(o => o.Payload)
+            .HasColumnType("jsonb")
+            .IsRequired();
+
+        builder.Property(o => o.OccurredOn)
+            .HasConversion(v => v.ToUniversalTime(),
+                v => DateTime.SpecifyKind(v, DateTimeKind.Utc))
+            .IsRequired();
+
+        builder.Property(o => o.ProcessedOn)
+            .HasConversion(v => v.Value.ToUniversalTime(),
+                v => DateTime.SpecifyKind(v, DateTimeKind.Utc))
+            .IsRequired(false);
+
+        builder.Property(o => o.Error)
+            .IsRequired(false);
+
+        builder.HasIndex(e => new
+        {
+            e.OccurredOn,
+            e.ProcessedOn
+        })
+            .HasDatabaseName("idx_outbox_messages_unprocessed")
+            .IncludeProperties(p => new
+            {
+                p.Id,
+                p.Type,
+                p.Payload,
+            })
+            .HasFilter("processed_on_utc IS NULL");
+    }
+}
