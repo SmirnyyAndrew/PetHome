79e510abbcbbe09fdff1019abb0cab30ebf49e24
Андрей Смирный <smirnay2001@mail.ru>
2025-03-10 14:43:40 +0300

    Добавил кэширование для получения пользователя по Id

--- src/Accounts/PetHome.Accounts.Application/Features/Read/GetUserInformation/GetUserInformationUseCase.cs
+++ src/Accounts/PetHome.Accounts.Application/Features/Read/GetUserInformation/GetUserInformationUseCase.cs
@@ -1,41 +1,49 @@
 ﻿using CSharpFunctionalExtensions;
-using FilesService.Core.Interfaces;
 using PetHome.Accounts.Application.Database.Repositories;
-using PetHome.Accounts.Application.Features.Read.GetUser;
 using PetHome.Accounts.Contracts.Dto;
 using PetHome.Accounts.Domain.Aggregates;
-using PetHome.Core.Extentions.ErrorExtentions;
+using PetHome.Core.Constants;
 using PetHome.Core.Interfaces.FeatureManagment;
+using PetHome.Core.Redis;
 using PetHome.Core.Response.Validation.Validator;
 
 namespace PetHome.Accounts.Application.Features.Read.GetUserInformation;
 public class GetUserInformationUseCase
-    : IQueryHandler<UserDto, GetUserInformationQuery>
+    : IQueryHandler<UserDto?, GetUserInformationQuery>
 {
     private readonly IAuthenticationRepository _repository;
+    private readonly ICacheService _cacheService;
 
-    public GetUserInformationUseCase(IAuthenticationRepository repository)
+    public GetUserInformationUseCase(
+        IAuthenticationRepository repository, ICacheService cacheService)
     {
         _repository = repository;
+        _cacheService = cacheService;
     }
 
-    public async Task<Result<UserDto, ErrorList>> Execute(
+    public async Task<Result<UserDto?, ErrorList>> Execute(
         GetUserInformationQuery query, CancellationToken ct)
     {
-        var result = await _repository.GetUserById(query.UserId, ct);
-        if (result.IsFailure)
-            return result.Error.ToErrorList();
+        UserDto? userDto = await _cacheService.GetOrSetAsync<UserDto>(Constants.Redis.USER(query.UserId), async () =>
+        {
+            var getUserResult = await _repository.GetUserById(query.UserId, ct);
+            if (getUserResult.IsFailure)
+                return null;
 
-        User user = result.Value;
+            User user = getUserResult.Value;
 
-        string roleName = user.Role is null ? string.Empty: user.Role.Name;
-        DateTime birthDate = user.BirthDate is null ? default: user.BirthDate.Value;
-        UserDto userDto = new UserDto(
-            user.Id,
-            user.UserName,
-            user.Email,
-            roleName,
-            birthDate);
+            string roleName = user.Role is null ? string.Empty : user.Role.Name;
+            DateTime birthDate = user.BirthDate is null ? default : user.BirthDate.Value;
+            userDto = new UserDto(
+                user.Id,
+                user.UserName,
+                user.Email,
+                roleName,
+                birthDate);
+
+            return userDto;
+        }, options: null, ct);
+
         return userDto;
     }
 }
