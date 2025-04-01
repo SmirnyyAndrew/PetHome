5fa3695acfedc7221fd1fe0f2a1e7eb08aad3e97
Андрей Смирный <smirnay2001@mail.ru>
2025-04-01 18:34:45 +0300

    Добавил AccountService

---
+++ AccountService/src/AccountService/AccountService.WEB/DI/ApplicationDI/MassTransitExtentions.cs
@@ -0,0 +1,11 @@
+﻿namespace AccountService.WEB.DI.ApplicationDI;
+public static class MassTransitExtentions
+{
+    public static IServiceCollection AddMassTransitConfig(
+        this IServiceCollection services, IConfiguration configuration)
+    {
+       
+
+        return services;
+    }
+}
---
+++ AccountService/src/AccountService/AccountService.WEB/DI/InfrastructureDependencyInjections.cs
@@ -0,0 +1,15 @@
+﻿using AccountService.WEB.DI.InfrastructureDI;
+
+namespace AccountService.WEB.DI;
+
+public static class InfrastructureDependencyInjections
+{
+    public static IServiceCollection AddInfrastructureDependencyInjection(
+        this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDatabases(configuration);
+        services.AddMessageBus(configuration);
+
+        return services;
+    }
+}
