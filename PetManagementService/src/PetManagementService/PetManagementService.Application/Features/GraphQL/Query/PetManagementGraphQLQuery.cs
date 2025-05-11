8218c7f298be1fe37be534b47d78b270712aa1a9
Андрей Смирный <smirnay2001@mail.ru>
2025-05-11 21:56:31 +0300

    GraphQL: удалил лишние атрибуты

--- PetManagementService/src/PetManagementService/PetManagementService.Application/Features/GraphQL/Query/PetManagementGraphQLQuery.cs
+++ PetManagementService/src/PetManagementService/PetManagementService.Application/Features/GraphQL/Query/PetManagementGraphQLQuery.cs
@@ -4,15 +4,10 @@ using PetManagementService.Application.Features.GraphQL.VolunteerAggregate;
 
 namespace PetManagementService.Application.Features.GraphQL.Query;
 public class PetManagementGraphQLQuery
-{
-    [UsePaging(typeof(PetGraphQLType))]
-    [HotChocolate.Data.UseFiltering]
+{ 
     public IQueryable<PetDto> Pets([Service] PetGraphQLService petService) 
         => petService.GetAll();
-
-
-    [UsePaging(typeof(VolunteerGraphQLType))]
-    [HotChocolate.Data.UseFiltering]
+     
     public IQueryable<VolunteerDto> Volunteers([Service] VolunteerGraphQLService volunteerService) 
         => volunteerService.GetAll();
 }
