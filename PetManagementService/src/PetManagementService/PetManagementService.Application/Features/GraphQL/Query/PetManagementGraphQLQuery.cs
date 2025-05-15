83d5d6da0ea6758863201ad5f561aa997b7041c8
Андрей Смирный <smirnay2001@mail.ru>
2025-05-15 16:33:27 +0300

    Добавил атрибуты фильтрации и сортировки в GraphQL

--- PetManagementService/src/PetManagementService/PetManagementService.Application/Features/GraphQL/Query/PetManagementGraphQLQuery.cs
+++ PetManagementService/src/PetManagementService/PetManagementService.Application/Features/GraphQL/Query/PetManagementGraphQLQuery.cs
@@ -4,10 +4,14 @@ using PetManagementService.Application.Features.GraphQL.VolunteerAggregate;
 
 namespace PetManagementService.Application.Features.GraphQL.Query;
 public class PetManagementGraphQLQuery
-{ 
+{
+    [HotChocolate.Data.UseFiltering]
+    [UseSorting]
     public IQueryable<PetDto> Pets([Service] PetGraphQLService petService) 
         => petService.GetAll();
-     
+
+    [HotChocolate.Data.UseFiltering]
+    [UseSorting]
     public IQueryable<VolunteerDto> Volunteers([Service] VolunteerGraphQLService volunteerService) 
         => volunteerService.GetAll();
 }
