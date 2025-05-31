6692407d3231e74f51c4330e87260be4b9638dc2
Андрей Смирный <smirnay2001@mail.ru>
2025-05-31 15:03:31 +0300

    Create mocker for AccountHttpClient

--- AccountService/src/AccountService.IntegrationTests/Features/Write/UpdateAccessTokenUsingRefreshToken/UpdateAccessTokenUsingRefreshTokenUseCaseTest.cs
+++ AccountService/src/AccountService.IntegrationTests/Features/Write/UpdateAccessTokenUsingRefreshToken/UpdateAccessTokenUsingRefreshTokenUseCaseTest.cs
@@ -1,40 +1,35 @@
-﻿//using AccountService.Application.Features.Write.UpdateAccessTokenUsingRefreshToken;
-//using AccountService.IntegrationTests.IntegrationFactories;
-//using Microsoft.Extensions.DependencyInjection;
-//using PetHome.Core.Application.Interfaces.FeatureManagement;
-//using PetHome.SharedKernel.Responses.Dto;
-//using Xunit;
+﻿using AccountService.Application.Features.Write.UpdateAccessTokenUsingRefreshToken;
+using AccountService.IntegrationTests.IntegrationFactories;
+using Microsoft.Extensions.DependencyInjection;
+using PetHome.Core.Application.Interfaces.FeatureManagement;
+using PetHome.SharedKernel.Responses.Dto;
+using Xunit;
 
-//namespace PetHome.Accounts.IntegrationTests.Features.Write.UpdateAccessTokenUsingRefreshToken;
-//public class UpdateAccessTokenUsingRefreshTokenUseCaseTest : AccountFactory
-//{
+namespace PetHome.Accounts.IntegrationTests.Features.Write.UpdateAccessTokenUsingRefreshToken;
+public class UpdateAccessTokenUsingRefreshTokenUseCaseTest : AccountFactory
+{
 
-//    private readonly ICommandHandler<TokenResponse, UpdateAccessTokenUsingRefreshTokenCommand> _sut;
+    private readonly ICommandHandler<TokenResponse, UpdateAccessTokenUsingRefreshTokenCommand> _sut;
 
-//    public UpdateAccessTokenUsingRefreshTokenUseCaseTest(IntegrationTestFactory factory) : base(factory)
-//    {
-//        _sut = _scope.ServiceProvider.GetRequiredService
-//            <ICommandHandler<TokenResponse, UpdateAccessTokenUsingRefreshTokenCommand>>();
-//    }
+    public UpdateAccessTokenUsingRefreshTokenUseCaseTest(IntegrationTestFactory factory) : base(factory)
+    {
+        _sut = _scope.ServiceProvider.GetRequiredService
+            <ICommandHandler<TokenResponse, UpdateAccessTokenUsingRefreshTokenCommand>>();
+    }
 
 
-//    [Fact]
-//    public async void Update_access_token_using_refresh_token()
-//    {
-//        //array  
-//        await SeedRoles();
-//        //RoleId roleId = _getRoleContract.Execute("admin", CancellationToken.None).Result.Value;
-//        ////var getUserIdResult = await _createUserContract.Execute(roleId, CancellationToken.None); 
-//        //var accessTokenResult = await _generateAccessTokenContract.Execute(getUserIdResult.Value, CancellationToken.None);
-//        //var refreshSessionResult = await _generateRefreshTokenContract.Execute(getUserIdResult.Value, accessTokenResult.Value, CancellationToken.None);
+    [Fact]
+    public async void Update_access_token_using_refresh_token()
+    {
+        //array  
+        await SeedRoles(); 
+        UpdateAccessTokenUsingRefreshTokenCommand command =
+            new (RefreshToken: Guid.NewGuid());
 
-//        UpdateAccessTokenUsingRefreshTokenCommand command =
-//            new UpdateAccessTokenUsingRefreshTokenCommand(refreshSessionResult.Value.RefreshToken);
+        //act
+        var result = await _sut.Execute(command, CancellationToken.None);
 
-//        //act
-//        var result = await _sut.Execute(command, CancellationToken.None);
-
-//        //assert
-//        Assert.True(result.IsSuccess);
-//    }
-//}
+        //assert
+        Assert.True(result.IsSuccess);
+    }
+}
---
+++ AccountService/src/AccountService.IntegrationTests/Mocks/AccountHttpClientMocker.cs
@@ -0,0 +1,92 @@
+﻿using AccountService.Contracts.HttpCommunication;
+using AccountService.Contracts.HttpCommunication.Dto;
+using Moq;
+using Moq.Protected;
+using System.Net;
+using System.Net.Http.Json;
+
+namespace AccountService.IntegrationTests.Mocks;
+public class AccountHttpClientMocker
+{
+    public static AccountHttpClient MockMethods()
+    {
+        var handlerMock = new Mock<HttpMessageHandler>();
+
+        handlerMock.Protected()
+            .Setup<Task<HttpResponseMessage>>("SendAsync",
+                ItExpr.Is<HttpRequestMessage>(req =>
+                    req.RequestUri!.AbsolutePath.StartsWith("/user-by-email")),
+                ItExpr.IsAny<CancellationToken>())
+            .ReturnsAsync((HttpRequestMessage request, CancellationToken token) =>
+            { 
+                return new HttpResponseMessage(HttpStatusCode.OK)
+                {
+                    Content = new StringContent("user@example.com")
+                };
+            });
+
+        handlerMock.Protected()
+            .Setup<Task<HttpResponseMessage>>("SendAsync",
+                ItExpr.Is<HttpRequestMessage>(req =>
+                    req.RequestUri!.AbsolutePath.StartsWith("/role-id-by-name")),
+                ItExpr.IsAny<CancellationToken>())
+            .ReturnsAsync(new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent(Guid.NewGuid().ToString())
+            });
+
+        handlerMock.Protected()
+            .Setup<Task<HttpResponseMessage>>("SendAsync",
+                ItExpr.Is<HttpRequestMessage>(req =>
+                    req.Method == HttpMethod.Post && req.RequestUri!.AbsolutePath == "/access-token"),
+                ItExpr.IsAny<CancellationToken>())
+            .ReturnsAsync(new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = JsonContent.Create("some-access-token")
+            });
+
+        handlerMock.Protected()
+            .Setup<Task<HttpResponseMessage>>("SendAsync",
+                ItExpr.Is<HttpRequestMessage>(req =>
+                    req.RequestUri!.AbsolutePath.StartsWith("/user-permissions-codes")),
+                ItExpr.IsAny<CancellationToken>())
+            .ReturnsAsync(new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = JsonContent.Create(new List<string> { "PERM_READ", "PERM_WRITE" })
+            });
+
+        handlerMock.Protected()
+            .Setup<Task<HttpResponseMessage>>("SendAsync",
+                ItExpr.Is<HttpRequestMessage>(req =>
+                    req.RequestUri!.AbsolutePath.StartsWith("/user-role-name")),
+                ItExpr.IsAny<CancellationToken>())
+            .ReturnsAsync(new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent("UserRoleName")
+            });
+
+        handlerMock.Protected()
+            .Setup<Task<HttpResponseMessage>>("SendAsync",
+                ItExpr.Is<HttpRequestMessage>(req =>
+                    req.RequestUri!.AbsolutePath.StartsWith("/user-main-information")),
+                ItExpr.IsAny<CancellationToken>())
+            .ReturnsAsync(new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = JsonContent.Create(new UserDto
+                (
+                    Guid.NewGuid(),
+                    "UserName",
+                    "user@domain.com",
+                    "admin",
+                    DateTime.UtcNow
+                ))
+            });
+
+        var httpClient = new HttpClient(handlerMock.Object)
+        {
+            BaseAddress = new Uri("http://test.api")
+        };
+
+        return new AccountHttpClient(httpClient);
+    }
+}
